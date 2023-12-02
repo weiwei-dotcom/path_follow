cb1242b470a924fd0c25a74b6d696ecf5c61f05c
weiwei-dotcom <728841808@qq.com>
2023-12-02 22:25:41 +0800

    Add the function of traversal path_points_list to get the deviation between path and cdcr

--- src/path_follow/src/cdcr.cpp
+++ src/path_follow/src/cdcr.cpp
@@ -37,8 +37,62 @@ CDCR::CDCR():Node("path_follow")
     return;
 }
 
-void CDCR::path_callback()
+void CDCR::path_follow_callback()
 {
-    ;
+    
+    return;
+}
+
+// Calculate the deviation of with path, which the deviation is the distance 
+// between cdcr_point and the point of intersection of cdcr_point's norm plane and path
+void CDCR::getPathDeviationAndNextIndex(const int path_point_index_start, 
+                                        const int cdcr_point_index, 
+                                        int& path_point_index_next_start)
+{
+    int temp_index = path_point_index_start;
+    Eigen::Vector3d direction = this->cdcr_points.col(cdcr_point_index)-this->path_points.col(temp_index);
+    std::cout << "here may cause error of eigen vector" << std::endl;
+    double direction_value = this->cdcr_point_tangent_vectors.col(cdcr_point_index).dot(direction);
+    if (direction_value < 0)
+    {
+        temp_index -= 1;
+        for (int i=temp_index; i>=0; i--)
+        {
+            Eigen::Vector3d last_direction = direction;
+            double last_direction_value = direction_value;
+            direction=this->cdcr_points.col(cdcr_point_index)-this->path_points.col(i);
+            direction_value = this->cdcr_point_tangent_vectors.col(cdcr_point_index).dot(direction);
+            if (direction_value > 0)
+            {
+                double temp_sum = direction_value + std::abs(last_direction_value);
+                Eigen::Vector3d interpolated_point = (temp_sum-direction_value)/temp_sum * this->path_points.col(i)
+                                                    +(direction_value/temp_sum) * path_points.col(i+1);
+                this->cdcr_point_deviation_values[cdcr_point_index] = (interpolated_point-cdcr_points.col(cdcr_point_index)).norm();
+                path_point_index_next_start = i+1;
+                return;
+            }
+        }
+    }
+    else
+    {
+        temp_index += 1;
+        int path_points_size = this->path_points.size();
+        for (int i=temp_index; i<=path_points_size; i++)
+        {
+            Eigen::Vector3d last_direction = direction;
+            double last_direction_value = direction_value;
+            direction=this->cdcr_points.col(cdcr_point_index)-this->path_points.col(i);
+            direction_value = this->cdcr_point_tangent_vectors.col(cdcr_point_index).dot(direction);
+            if (direction_value < 0)
+            {
+                double temp_sum = last_direction_value + std::abs(direction_value);
+                Eigen::Vector3d interpolated_point = (temp_sum-last_direction_value)/temp_sum * this->path_points.col(i-1)
+                                                    +(last_direction_value/temp_sum) * path_points.col(i);
+                this->cdcr_point_deviation_values[cdcr_point_index] = (interpolated_point-cdcr_points.col(cdcr_point_index)).norm();
+                path_point_index_next_start = i;
+                return;
+            }
+        }
+    }
     return;
 }
--- src/path_follow/src/cdcr.hpp
+++ src/path_follow/src/cdcr.hpp
@@ -11,8 +11,13 @@ class CDCR:public rclcpp::Node
 {
 public:
 CDCR();
-
+void getPathDeviationAndNextIndex(const int path_point_index_start, 
+                                  const int cdcr_point_index, 
+                                  int& path_point_index_next_start);
 private:
+vector<double> cdcr_point_deviation_values;
+Eigen::MatrixXd cdcr_points, cdcr_point_tangent_vectors;
+Eigen::MatrixXd path_points;
 int joint_number;
 std::vector<Joint> joints;
 std::vector<Eigen::Matrix4d> transform_joint_to_base;
@@ -20,7 +25,7 @@ Eigen::Matrix4d transform_base_to_world, transform_world_to_base;
 rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub;
 
 
-void path_callback();
+void path_follow_callback();
 
 
 };
