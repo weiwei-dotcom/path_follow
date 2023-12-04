d6b4fcd4d33c5c451d74400675a1c7ab9aa74636
weiwei-dotcom <728841808@qq.com>
2023-12-04 21:22:42 +0800

    start code the function of fitCDCR()

--- src/path_follow/src/joint.hpp
+++ src/path_follow/src/joint.hpp
@@ -11,7 +11,7 @@ std::vector<double> max_deviations;
 std::vector<double> current_deviations;
 std::vector<double> path_deviations;
 std::vector<std::vector<Eigen::Vector2d>> joint_variate_at_max_deviation;
-std::vector<Eigen::Matrix4d> joint_variate_at_max_deviation;
+std::vector<Eigen::Matrix4d> transform_at_max_deviation;
 Eigen::MatrixXd joint_points,joint_points_tangent_vectors;
 double length;
 double length_rigid1,length_rigid2,length_continuum;
